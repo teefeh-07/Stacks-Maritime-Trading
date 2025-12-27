55803d20171e78caf2496a1723db2067e172f0da
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-27 15:38:35 +0100

    test: assert cargo ID returned

--- tests/trade-manager_test.ts
+++ tests/trade-manager_test.ts
@@ -3,3 +3,4 @@ import { Clarinet, Tx, Chain, Account, types } from "https://deno.land/x/clarine
 Clarinet.test({ name: "Can book cargo", async fn(chain: Chain, accounts: Map<string, Account>) {
   const user = accounts.get("wallet_1")!;
   let block = chain.mineBlock([Tx.contractCall("trade-manager", "book-cargo", [types.uint(1), types.ascii("Electronics")], user.address)]);
+  block.receipts[0].result.expectOk().expectUint(1);
