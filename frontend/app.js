de18015eca50936edc9499cd02e2df381e7a1404
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-27 15:20:50 +0100

    feat: initialize user session

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,4 @@
 import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 
 const appConfig = new AppConfig(["store_write", "publish_data"]);
+const userSession = new UserSession({ appConfig });
