b8b5a8740101ed2418cfc35ccd82827193ead411
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-27 15:20:59 +0100

    feat: add connectWallet function

--- frontend/app.js
+++ frontend/app.js
@@ -2,3 +2,5 @@ import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 
 const appConfig = new AppConfig(["store_write", "publish_data"]);
 const userSession = new UserSession({ appConfig });
+
+function connectWallet() {
