229c6c3d6ea9bcc56c1355cd20818764b1640c9c
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-27 15:38:03 +0100

    test: get test user

--- tests/trade-manager_test.ts
+++ tests/trade-manager_test.ts
@@ -1,3 +1,4 @@
 import { Clarinet, Tx, Chain, Account, types } from "https://deno.land/x/clarinet@v1.0.0/index.ts";
 
 Clarinet.test({ name: "Can book cargo", async fn(chain: Chain, accounts: Map<string, Account>) {
+  const user = accounts.get("wallet_1")!;
